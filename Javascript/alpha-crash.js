30ae605f8eba0d3d7c7946ffc5794ae00a214e88
ANTO <faruquerahman03@gmail.com>
2024-03-01 08:02:54 +0600

    last commit\

--- Javascript/alpha-crash.js
+++ Javascript/alpha-crash.js
@@ -1,3 +1,24 @@
-function play() {
-    console.log('play start now');
+function play(){
+    hideElementbyId('home-screen');
+    showElementById('play-ground');
+    continueGame();
 }
+function continueGame(){
+    // Generate Random Alphabet
+    const alphabet = getARandomAlphabet();
+    // set randomly generated alphabet to the screen (show it)
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+    setBackgroundColorbyId(alphabet);
+}
+
+
+// function play() {
+// //   step-1: Hide the home screen. to hide the screen add the class to the home
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+
+// // show the playground
+// const playgroundSection = document.getElementById('play-ground');
+// playgroundSection.classList.remove('hidden');
+// }
